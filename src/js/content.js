6f451ceb37756ba16e8d071f8a73b2ee7a4e1883
Takuto Yanagida <yanagida@stxst.com>
2021-12-07 23:13:23 +0900

    Refactoring

--- src/js/content.js
+++ src/js/content.js
@@ -3,35 +3,37 @@
  * Content
  *
  * @author Takuto Yanagida
- * @version 2021-11-19
+ * @version 2021-12-07
  *
  */
 
+
+'use strict';
 
 window['NACSS'] = window['NACSS'] || {};
 
 
 (function (NS) {
 
-	(function () {
+	{
 		// @include _image-alt.js
 		NS.contentImageAlt = initialize;
-	})();
+	}
 
-	(function () {
+	{
 		// @include _figure-caption.js
 		NS.contentFigureCaption = initialize;
-	})();
+	}
 
-	(function () {
+	{
 		// @include _iframe-aspect.js
 		NS.contentIframeAspect = initialize;
-	})();
+	}
 
-	(function () {
+	{
 		// @include _underline.js
 		NS.contentUnderline = initialize;
-	})();
+	}
 
 	// @include _style-class.js
 
